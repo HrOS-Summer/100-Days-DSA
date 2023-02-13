dc9e78105db9f0a355ea15e0848f88dc90a5081e
unknown <21bcs5872@cuchd.in>
2023-02-13 22:10:42 +0530

    Day 1 bubbleSort

---
+++ BubbleSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {  //S
+                int temp = arr[j];      //W
+                arr[j] = arr[j + 1];    //A
+                arr[j + 1] = temp;      //P
+            }
+        }
+    }
+}
+
+int main(){
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+    bubbleSort(arr, size);
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
