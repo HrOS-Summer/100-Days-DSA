3c69b738fa0ad845bdf1423820a6e94a44c66f9e
Hariom Singh <21bcs5872@cuchd.in>
2023-02-16 18:46:58 +0530

    modified firstLastOccurance.cpp

--- firstLastOccurance.cpp
+++ firstLastOccurance.cpp
@@ -1,9 +1,10 @@
 //Finding first and last index of an element in a sorted array
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int first_occurance(int arr[], int size, int key) {
+int first_occurance(vector <int> &v, int size, int key) {
     int s = 0; int e = size - 1;
     int mid = s + (e-s)/2;
     int ans = -1;
@@ -45,13 +46,21 @@ int last_occurance(int arr[], int size, int key) {
     return ans;
 }
 
+pair<int, int>firstAndLastPosition (vector<int> &arr, int size, int key) {
+    pair<int, int> p;
+    p.first = first_occurance(arr, size, key);
+    p.second = last_occurance(arr, size, key);
+
+    return p;
+}
+
 int main()
 {
-    int arr[10] = {1,2,2,2,3,3,3,3,4,5};
+    vector = {1,2,2,2,3,3,3,3,4,5};
     int index = first_occurance(arr, 10, 3);
     int lindex = last_occurance(arr, 10, 3);
-    cout<<index<<endl;
-    cout<<lindex;
+    cout<<p.first<<endl;
+    cout<<p.second;
 
     return 0;
 }
