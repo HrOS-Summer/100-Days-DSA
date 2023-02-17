b7abb69f4b2e7b25da7dd578dc3bca10e1399d7b
Hariom Singh <21bcs5872@cuchd.in>
2023-02-17 10:04:56 +0530

    noOfOccurance.cpp calculates the frequency of an element in array

---
+++ noOfOccurance.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//If array is not sorted then use linear search
+
+int first_occurance(vector <int> &v, int size, int key) {
+    int s = 0; int e = size - 1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    
+    while (s<=e) {
+        if(v[mid] == key) {
+            ans = mid;
+            e = mid - 1;
+        }
+        
+        else if(key < v[mid])
+            e = mid-1;
+        else
+            s = mid+1;
+            
+        mid = s+(e-s)/2;
+    }
+    return ans;
+}
+
+int last_occurance(vector <int> &v, int size, int key) {
+    int s = 0; int e = size - 1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    
+    while (s<=e) {
+        if(v[mid] == key) {
+            ans = mid;
+            s = mid + 1;
+        }
+        
+        else if(key < v[mid])
+            e = mid-1;
+        else
+            s = mid+1;
+            
+        mid = s+(e-s)/2;
+    }
+    return ans;
+}
+
+int noOfOccurance(vector<int> &v, int size, int key) {
+	return (last_occurance(v, size, key) - first_occurance(v, size, key)) + 1;
+}
+
+int main() {
+	vector<int> arr = {10, 10, 12, 15, 16, 16, 16, 16, 20, 56};
+	int total = noOfOccurance(arr, 10, 16);
+	cout<<"16 occured "<<total<<" times";
+}
